05dd6f5b573b0a4eddce6e147cfafdeb0acacf43
Carl Holms <holmscarl@gmail.com>
2021-05-12 09:52:31 +0200

    Select colors

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,23 @@
 // index.html
-const content = document.querySelector('.content')
+const content = document.getElementById('content')
 
 function productList(products) {
-  for(let elem of products) {
-    content.innerHTML += `<h1 id="name">${elem.name}</h1><p id="price">
-    ${elem.price/100} €</p><button><a href="/pages/produit.html?_id=${elem._id}">Voir le produit</a></button>`
+  for(elem of products) {
+    content.innerHTML += `
+    <h1 id="name">${elem.name}</h1>
+    <p id="price">${elem.price/100} €</p>
+    <button><a href="/pages/produit.html?_id=${elem._id}">Voir le produit</a></button>
+    `
   }
 }
 
 async function retrieveProducts() {
-  await fetch('http://localhost:3000/api/teddies')
+  await fetch('https://oc-p5-api.herokuapp.com/api/teddies')
     .then((response) => response.json()) 
     .then((products) => productList(products))
 }
 
 retrieveProducts()
-
 
 //produit.html**********************************
 
@@ -25,30 +27,32 @@ const params = new URLSearchParams(window.location.search)
 const result = params.get('_id')
 
 function teddyId(teddies) {
-  for(let teddy of teddies) {
+  for(teddy of teddies) {
     if(result ===  teddy._id) {
       const image = document.createElement('img')
       document.querySelector('.product').appendChild(image)
       image.src =  `${teddy.imageUrl}`
 
-      product.innerHTML += `<h1 id="name">${teddy.name}</h1><p id="description">${teddy.description}</p><p id="price">${teddy.price/100}€</p>
-      <p id="colors">Couleurs : ${teddy.colors}</p>`
+      product.innerHTML += `
+      <h1 id="name">${teddy.name}</h1>
+      <p id="description">${teddy.description}</p>
+      <p id="price">${teddy.price/100}€</p>
+      <select>
+      <option value="">${teddy.colors[0]}</option>
+      <option value="">${teddy.colors[1]}</option>
+      <option value="">${teddy.colors[2]}</option>
+      <option value="">${teddy.colors[3]}</option>
+      </select>
+      `
     }
   }
 }
-
 async function retrieveTeddies() {
-  await fetch('http://localhost:3000/api/teddies')
+  await fetch('https://oc-p5-api.herokuapp.com/api/teddies')
   .then((response) => response.json())
   .then((teddies) => teddyId(teddies))
 }
 
 retrieveTeddies()
 
-//panier.html**********************************
-
-const btnSelect = document.getElementById('btnSelect')
-
-btnSelect.onclick = function() { 
-  alert('Added to cart')
- }
+//panier.html*********************************
