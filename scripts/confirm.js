a31767dd06c5a4ccf14121c0332173d39febd881
Carl Holms <holmscarl@gmail.com>
2021-06-10 12:29:41 +0200

    confirm page done

--- scripts/confirm.js
+++ scripts/confirm.js
@@ -1,16 +1,20 @@
-function displayTotalPrice() {
+function displayOrderInfo() {
+    // retrieve total from localStorage
     const total = localStorage.getItem('total')
+    const lastName = localStorage.getItem('lastName')
+    const firstName = localStorage.getItem('firstName')
+    const orderId = localStorage.getItem('orderId')
 
-    const renameMeLater = `
+    const displayrecap = `
         <div id="containerRecapOrder">
             <h2>Récaplitulatif de votre commande</h2>
             <div class="recapOrder">
-                <p>Merci <span class="bold">Nom</span> <span class="bold">Prénom</span> pour votre commande</p>
-                <p>Votre numéro de commande est : <span class="bold">xxxx</span></p>
-                <p>Le montant de votre commande est : <span class="bold">${total}</span> €</p>
+                <p>Merci <span class="bold">${lastName}</span> <span class="bold">${firstName}</span> pour votre commande</p>
+                <p>Le montant total de votre commande est : <span class="bold">${total}</span> €</p>
+                <p>Votre numéro de commande est : <span class="bold">${orderId}</span></p>
             </div>
         </div>
     `
-    document.getElementById('recapDisplay').innerHTML = renameMeLater
+    document.getElementById('recapDisplay').innerHTML = displayrecap
 }
-displayTotalPrice()
+displayOrderInfo()
